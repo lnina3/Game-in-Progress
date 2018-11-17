d373f52d9dd8e2324e23cfffc12fc7805255bed6
lnina3 <44215384+lnina3@users.noreply.github.com>
2018-11-17 11:06:57 -0800

    Add files via upload

--- 1test.js
+++ 1test.js
@@ -1,17 +1,17 @@
 var myGamePiece;
 
-funtion startGame(){
+function startGame(){
 	myGameArea.start();
 	myGamePiece = new component (20, 20, "red" , 10, 120);
 }
-//var c = document.getElementById("myGameArea");
+
 var myGameArea = {
 	canvas : document.createElement("canvas"),
 	start : function() {
 		this.canvas.width = 900;
-		this.canvas.width = 500;
+		this.canvas.height = 500;
 		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, docuent.vid,childNodes[0]);
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	}
 
 }
@@ -19,7 +19,7 @@ var myGameArea = {
 function component(width, height, color, x, y){
 	this.width = width;
 	this.height = height;
-	this.x=x;
+	this.x = x;
 	this.y = y;
 	ctx = myGameArea.context;
 	ctx.fillStyle = color;
